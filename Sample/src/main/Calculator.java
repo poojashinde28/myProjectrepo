c53bae57e5ea78f83b42ddfef41c7de0e694359f
poojashinde28 <30907692+poojashinde28@users.noreply.github.com>
2021-03-22 18:29:47 +0530

    updated

--- Sample/src/main/Calculator.java
+++ Sample/src/main/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helo");
+		System.out.println("hello world");
 		add(10,20);
 
 	}
