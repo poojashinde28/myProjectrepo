82e8838210bf7d50cbd4b0f717f7a02e3c9f05bb
poojashinde28 <shindepooja2899>
2021-03-23 10:14:01 +0530

    merged sq

--- Sample/src/main/Calculator.java
+++ Sample/src/main/Calculator.java
@@ -24,7 +24,7 @@ public class Calculator {
 	}
 	public static void mod(int num1,int num2){
 		System.out.println(num1%num2);
-
+	}
 	public static void sq(int num1,int num2){
 		System.out.println(num1*num2);
 
